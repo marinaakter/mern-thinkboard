f309e9165e69afdaff8af63670db8a7aa026b8f4
Marina <marinaakter878@gmail.com>
2025-11-26 19:58:24 +0600

    feat: add daisyUI v5.5.5 with new utilities for join, radius, and typography
    - Implemented join utility with associated styles and JavaScript logic.
    - Added radius utility for consistent border-radius management.
    - Introduced typography utility for enhanced text styling.
    - Updated package.json to include daisyUI version 5.5.5.
    - Modified App component to comment out toast notification for future use.
    - Updated index.css to include daisyUI plugin.
    - Cleaned up HomePage component formatting.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import toast from "react-hot-toast";
+// import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Congrates")}>Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/create" element={<CreatePage />}></Route>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div>
       
     </div>
-  )
+  );
 }
 
 export default HomePage
