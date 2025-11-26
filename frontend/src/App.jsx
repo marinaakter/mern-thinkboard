9b674de74e115747de12abfc56b373a9776b0d89
Marina <marinaakter878@gmail.com>
2025-11-26 19:47:04 +0600

    Add toast notifications and improve code formatting
    - Integrated react-hot-toast for success notifications in App component.
    - Added a button to trigger a success toast message.
    - Improved code formatting for better readability in App.jsx and main.jsx.
    - Wrapped Toaster component in main.jsx to display toast notifications globally.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import CreatePage from "./pages/CreatePage"
-import NoteDetailPage from "./pages/NoteDetailPage"
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success("Congrates")}>Click me</button>
       <Routes>
-        <Route path="/" element={ <HomePage/> } ></Route>
-        <Route path="/create" element={<CreatePage />} ></Route>
-        <Route path="/note/:id" element={ <NoteDetailPage/> } ></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+        <Route path="/note/:id" element={<NoteDetailPage />}></Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router'
+import { BrowserRouter } from 'react-router'
+import {Toaster} from 'react-hot-toast'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <div>
+        <Toaster/>
+      </div>
     </BrowserRouter>
   </StrictMode>
 );
