8feab1c869afa4e7e6c5d7d09e4db8ede4318712
Marina <marinaakter878@gmail.com>
2025-11-26 19:41:22 +0600

    Add routing for Home, Create, and Note Detail pages in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import NoteDetailPage from "./pages/NoteDetailPage"
+
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={ <HomePage/> } ></Route>
+        <Route path="/create" element={<CreatePage />} ></Route>
+        <Route path="/note/:id" element={ <NoteDetailPage/> } ></Route>
+      </Routes>
     </div>
   )
 }
